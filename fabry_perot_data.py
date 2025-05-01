ed8a4f120c8e1f85e39e69a5de9a7a2a4ea87c9b
Julia White <julia.17.white@gmail.com>
2025-05-01 10:11:05 -0400

    first time calculation method

--- fabry_perot_data.py
+++ fabry_perot_data.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import scipy
 
 # reading in the csv
 df = pd.read_csv('scope_1.csv')
@@ -15,8 +16,9 @@ print(df.head())
 
 plt.figure()
 plt.plot(df['x-axis'], df['2'])
-plt.plot(df['x-axis'], df['1'])
-plt.show()
+plt.xlabel('time (s)')
+plt.ylabel('voltage')
+# plt.plot(df['x-axis'], df['1'])
 
 
 #################################################################
@@ -24,3 +26,31 @@ plt.show()
 # they should move from less complex to more complex
 
 #################################################################
+# using the scipy peak finding algorithm to locate the peaks
+peak_width = 10
+peaks = scipy.signal.find_peaks(df['2'], width=peak_width)
+print(peaks[1])
+plt.figure()
+plt.plot(df['x-axis'], df['2'])
+plt.scatter(df['x-axis'][peaks[0] + 8], peaks[1]['prominences'] + 0.36, c = 'm', s=15)
+# the +8 is to account for the NaN indices which were removed
+# the +36 is a y-axis normalization
+plt.xlabel('time (s)')
+plt.ylabel('voltage')
+
+
+#################################################################
+# now i'll take the two middle peaks and calculate delta t with them
+time_diff = df['x-axis'][peaks[0][int(len(peaks[0])/2)] + 8] - df['x-axis'][peaks[0][int(len(peaks[0])/2) - 1] + 8]
+print(f'the time difference is {time_diff:.4f} seconds')
+
+#################################################################
+# instead i can plot the times vs frequency and take the linear part of the polynomial
+plt.figure()
+plt.scatter(df['x-axis'][peaks[0][2:-1]], peaks[1]['prominences'][2:-1] + 0.36, s=15)
+
+
+
+
+
+plt.show()
